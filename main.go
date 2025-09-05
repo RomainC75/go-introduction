55f5975357d6a4649e5590b9c8ea014c719b9894
Romain CHENARD <romainchenard@Romains-MacBook-Pro.local>
2025-09-05 16:54:01 +0200

    feat: 🎸 copy() - limit by length

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import "fmt"
 
 func main() {
 
-	slc1 := []int{1, 2, 3, 4}
-	fmt.Printf("%p : len : %d // cap : %d \n", slc1, len(slc1), cap(slc1))
+	slc1 := []int{1, 2, 3, 4, 5}
+	slc2 := make([]int, 4, 5)
+	fmt.Printf("slc1 : %p / len : %v / cap : %v \n", slc1, len(slc1), cap(slc1))
+	fmt.Printf("PRE slc2 : %p / len : %v / cap : %v \n", slc2, len(slc2), cap(slc2))
 
-	slc1 = append(slc1[:2], slc1[3:]...)
-	fmt.Printf("%p : len : %d // cap : %d \n", slc1, len(slc1), cap(slc1))
-	fmt.Println(slc1)
+	nbCopied := copy(slc2, slc1)
+	fmt.Println("copied : ", nbCopied)
+	fmt.Printf("POST slc2 : %p / len : %v / cap : %v \n", slc2, len(slc2), cap(slc2))
 }
 
 func displayMatrix(matrix [4][4]int) {
