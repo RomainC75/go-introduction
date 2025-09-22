ec7e4b16e7bb753b41aaa7f98b1ce875fafaf943
Romain CHENARD <romainchenard@Romains-MacBook-Pro.local>
2025-09-22 11:17:08 +0200

    test: 💍 benchmark merge_sort

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"slices"
-)
-
 func main() {
-	slc1 := []int{1, 4, 6, 23, 54, 100, 110}
-	slc2 := []int{3, 6, 32, 115, 342}
-	res := make([]int, 0, len(slc1)+len(slc2))
-	res = append(res, slc1...)
-	res = append(res, slc2...)
+	// slc1 := []int{1, 4, 6, 23, 54, 100, 110}
+	// slc2 := []int{3, 6, 32, 115, 342}
+	// res := make([]int, 0, len(slc1)+len(slc2))
+	// res = append(res, slc1...)
+	// res = append(res, slc2...)
 
-	slices.SortFunc(res, func(a, b int) int {
-		return a - b
-	})
+	// slices.SortFunc(res, func(a, b int) int {
+	// 	return a - b
+	// })
 
-	fmt.Println(res)
+	// fmt.Println(res)
 }
