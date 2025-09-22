4478a3fba514d93c089cbbc0bee3d9f5686b1e74
Romain CHENARD <romainchenard@Romains-MacBook-Pro.local>
2025-09-22 09:33:14 +0200

    feat: 🎸 merge 2 sorted slices + library

--- main.go
+++ main.go
@@ -1,30 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 	slc1 := []int{1, 4, 6, 23, 54, 100, 110}
 	slc2 := []int{3, 6, 32, 115, 342}
 	res := make([]int, 0, len(slc1)+len(slc2))
+	res = append(res, slc1...)
+	res = append(res, slc2...)
 
-	i1 := 0
-	i2 := 0
+	slices.SortFunc(res, func(a, b int) int {
+		return a - b
+	})
 
-	for i1 < len(slc1)-1 && i2 < len(slc2)-1 {
-		if slc1[i1] < slc2[i2] {
-			res = append(res, slc1[i1])
-			i1++
-		} else {
-			res = append(res, slc2[i2])
-			i2++
-		}
-	}
-
-	if i1 < len(slc1)-1 {
-		res = append(res, slc1[i1:]...)
-	}
-	if i2 < len(slc2)-1 {
-		res = append(res, slc2[i2:]...)
-	}
 	fmt.Println(res)
 }
